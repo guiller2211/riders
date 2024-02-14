8875003e019ccfaf64e34c3f1cd509198ad319a5
Guillermo Araujo <guillermo.araujo@smithcommerce.com>
2024-02-14 12:53:17 -0300

    add

--- apps/ducati/utils/supabase.tsx
+++ apps/ducati/utils/supabase.tsx
@@ -1,8 +1,7 @@
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
 
-const supabaseUrl: string = process.env.SUPABASE_URL!;
-const supabaseKey: string = process.env.SUPABASE_ANON_KEY!;
-
+const supabaseUrl: string = process.env.SUPABASE_URL || '';
+const supabaseKey: string = process.env.SUPABASE_ANON_KEY || '';
 const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
