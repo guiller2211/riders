08b363e4f49142a916670dabcc4a644e8c793a1e
Guillermo Araujo <guillermo.araujo@smithcommerce.com>
2024-02-14 12:38:55 -0300

    agregando nuevo function

--- apps/ducati/utils/supabase.js
+++
@@ -1,4 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://mygdufjetbyenygxcgiv.supabase.co'
-const supabaseKey = process.env.SUPABASE_ANON_KEY
-export const supabase = createClient(supabaseUrl, supabaseKey)
